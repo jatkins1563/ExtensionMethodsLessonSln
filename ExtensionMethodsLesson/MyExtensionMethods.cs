3186587085a67c0fc492275bc4fc6d73df15ec5e
John Atkins <jdatkins@tql.com>
2021-05-25 17:19:04 -0400

    Add project files.

---
+++ ExtensionMethodsLesson/MyExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethodsLesson
+{
+    static class MyExtensionMethods
+    {
+        public static string ToUpperCase(this string str)
+        {
+            return str.ToUpper();
+        }
+        public static void ToConsole(this string str)
+        {
+            Console.WriteLine($"The string is {str}.");
+        }
+    }
+}
---
+++ ExtensionMethodsLesson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethodsLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abc = "abc";
+            abc.ToUpperCase().ToConsole();
+        }
+    }
+}
